7e47f2d833cebc4870318d8580b5f59ef9286b9d
cameron-yee <yee.camero23@gmail.com>
2020-04-22 13:26:19 -0600

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,59 @@
 import Head from 'next/head'
 
-export default function Home() {
+import { GetStaticProps } from 'next'
+
+import fs from 'fs'
+import path from 'path'
+
+export const getStaticProps: GetStaticProps = async context => {
+  const parables_path = path.join(process.cwd(), 'parables.json')
+  const parables = JSON.parse(fs.readFileSync(parables_path, 'utf8'))  
+
+  return {
+    props: {
+      parables
+    }
+  }
+}
+
+const IndexPage = ({ parables }) => {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Parables</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Parables
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
+          { parables.map((parable, index) => {
+              return (
+                <a href={`/parable/${parable.title.toLowerCase().replace(/\s/g, '-')}`} className="card" key={`parable-${index}`}>
+                  <h3>{ parable.title }</h3>
+                  { parable.startVerse !== parable.endVerse
+                    ?
+                    <p className="verse">{parable.book} {parable.chapter}:{parable.startVerse}-{parable.endVerse}</p>
+                    :
+                    <p className="verse">{parable.book} {parable.chapter}:{parable.startVerse}</p>
+                  }
+                </a>
+              )
+            })
+          }
         </div>
       </main>
 
       <footer>
         <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/cameron-yee"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+          <img src="/github.svg" alt="GitHub Logo" style={{width: '64px'}} />
         </a>
       </footer>
 
@@ -139,40 +138,46 @@ export default function Home() {
 
         .grid {
           display: flex;
-          align-items: center;
+          align-items: stretch;
           justify-content: center;
           flex-wrap: wrap;
 
-          max-width: 800px;
+          max-width: 1200px;
           margin-top: 3rem;
+        }
+
+        .verse {
+          position: absolute;
+          bottom: 10%;
         }
 
         .card {
           margin: 1rem;
-          flex-basis: 45%;
+          flex-basis: 20%;
           padding: 1.5rem;
           text-align: left;
           color: inherit;
           text-decoration: none;
           border: 1px solid #eaeaea;
           border-radius: 10px;
+          position: relative;
           transition: color 0.15s ease, border-color 0.15s ease;
+          width: 90%;
         }
 
         .card:hover,
         .card:focus,
         .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
+          border-color: black;
         }
 
         .card h3 {
-          margin: 0 0 1rem 0;
+          margin: 0 0 2rem 0;
           font-size: 1.5rem;
         }
 
         .card p {
-          margin: 0;
+          margin: .5rem 0 0 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
@@ -202,3 +207,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default IndexPage
---
+++ pages/parable/[title].tsx
@@ -0,0 +1,237 @@
+import Head from 'next/head'
+
+import fetch from 'node-fetch'
+
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+import fs from 'fs'
+import path from 'path'
+
+const Parable = ({parable, parable_content}) => {
+  return (
+    <div className="container">
+      <Head>
+        <title>Parables | {parable.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <a href="/"><button>Back to Home</button></a>   
+        <h1 className="title">{parable.title}</h1>
+        { parable.startVerse !== parable.endVerse
+          ?
+          <h2>{parable.book} {parable.chapter}:{parable.startVerse}-{parable.endVerse}</h2>
+          :
+          <h2>{parable.book} {parable.chapter}:{parable.startVerse}</h2>
+        }
+        <div className="grid">
+          <p className="description">{parable_content.text}</p>
+        </div>
+      </main>
+      <footer>
+        <a
+          href="https://github.com/cameron-yee"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/github.svg" alt="GitHub Logo" style={{width: '64px'}} />
+        </a>
+      </footer>
+      <style jsx>{`
+        button {
+          border: 1px solid black;
+          background: black;
+          color: white;
+          cursor: pointer;
+          border-radius: .25rem;
+          font-size: 1.1rem;
+          margin-bottom: 1rem;
+          padding: .5rem;
+        }
+
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+          text-align: left;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: start;
+          flex-wrap: wrap;
+
+          max-width: 1200px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export const getStaticPaths: any = () => {
+  const parables_path = path.join(process.cwd(), 'parables.json')
+  const parables = JSON.parse(fs.readFileSync(parables_path, 'utf8'))
+  
+  const paths = parables.map((parable) => `/parable/${parable.title.toLowerCase().replace(/\s/g, '-')}`)
+  
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  const parables_path = path.join(process.cwd(), 'parables.json')
+  const parables = JSON.parse(fs.readFileSync(parables_path, 'utf8'))
+
+  let i = 0
+  for (; i < parables.length; i++) {
+    if (parables[i].title.toLowerCase().replace(/\s/g, '-') == params.title) {
+      break 
+    }
+  }
+
+  const parable = parables[i]
+  const is_parable_one_verse = parable.startVerse === parable.endVerse
+
+  let res
+  if (is_parable_one_verse) {
+    res = await fetch(`https://bible-api.com/${parable.book.toLowerCase()}%20${parable.chapter}:${parable.startVerse}`)
+  } else {
+    res = await fetch(`https://bible-api.com/${parable.book.toLowerCase()}+${parable.chapter}:${parable.startVerse}-${parable.endVerse}`)
+  }
+  
+  const parable_content = await res.json()
+
+  return {
+    props: {
+      parable,
+      parable_content
+    }
+  }
+}
+
+export default Parable
